f99ae9e1d7874e67b5b5f7135841b9404c7d22f2
Vidhu Appalaraju <vidhuappalaraju@gmail.com>
2018-09-20 19:03:27 -0500

    added search bar and completed assignment 3

--- Yelp/Business.swift
+++ Yelp/Business.swift
@@ -65,31 +65,31 @@ class Business: NSObject {
         let rating = dictionary["rating"] as? Double
         if rating != nil {
             switch rating {
-                case 1:
+                case 1?:
                     self.ratingImage = UIImage(named: "stars_1")
                     break
-                case 1.5:
+                case 1.5?:
                     self.ratingImage = UIImage(named: "stars_1half")
                     break
-                case 2:
+                case 2?:
                     self.ratingImage = UIImage(named: "stars_2")
                     break
-                case 2.5:
+                case 2.5?:
                     self.ratingImage = UIImage(named: "stars_2half")
                     break
-                case 3:
+                case 3?:
                     self.ratingImage = UIImage(named: "stars_3")
                     break
-                case 3.5:
+                case 3.5?:
                     self.ratingImage = UIImage(named: "stars_3half")
                     break
-                case 4:
+                case 4?:
                     self.ratingImage = UIImage(named: "stars_4")
                     break
-                case 4.5:
+                case 4.5?:
                     self.ratingImage = UIImage(named: "stars_4half")
                     break
-                case 5:
+                case 5?:
                     self.ratingImage = UIImage(named: "stars_5")
                     break
                 default:
--- Yelp/BusinessesViewController.swift
+++ Yelp/BusinessesViewController.swift
@@ -8,16 +8,24 @@
 
 import UIKit
 
-class BusinessesViewController: UIViewController {
+class BusinessesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     var businesses: [Business]!
     
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 120
+        let searchBar = UISearchBar()
+        searchBar.sizeToFit()
+        navigationItem.titleView = searchBar
         Business.searchWithTerm(term: "Thai", completion: { (businesses: [Business]?, error: Error?) -> Void in
             
                 self.businesses = businesses
+                self.tableView.reloadData()
                 if let businesses = businesses {
                     for business in businesses {
                         print(business.name!)
@@ -43,6 +51,22 @@ class BusinessesViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if businesses != nil {
+            return businesses.count
+        }
+        else{
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BusinessCell", for: indexPath) as! BusinessCell
+        
+        cell.business = businesses[indexPath.row]
+        return cell
     }
     
     /*
